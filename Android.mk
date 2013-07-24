95f92ab910041bed1c785db2646086017ad3d553
Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
2013-07-24 02:01:14 -0300

    Making it static and installed at recovery/sbin, so it can be used on early-init
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>

--- Android.mk
+++ Android.mk
@@ -6,15 +6,11 @@ LOCAL_SRC_FILES := upstart-property-watcher.c
 LOCAL_C_INCLUDES := \
 	bionic/libc/bionic
 
-LOCAL_STATIC_LIBRARIES := \
-	libc \
-	libdl \
-	liblog
-
-LOCAL_SHARED_LIBRARIES := \
-	libcutils
-
 LOCAL_MODULE := upstart-property-watcher
-LOCAL_MODULE_TAGS := optional
 
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+
+LOCAL_STATIC_LIBRARIES := libcutils libc liblog
 include $(BUILD_EXECUTABLE)
